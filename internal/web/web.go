ef09e2c65ac2004efcbd77842b7ec903b557eb40
Kaamkiya <ziam3.1415@gmail.com>
2024-12-25 08:35:49 -0500

    fix: Multiple returns

--- internal/web/web.go
+++ internal/web/web.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	if err := w.Write([]byte("<!DOCTYPE html>This is in progress.")); err != nil {
+	if _, err := w.Write([]byte("<!DOCTYPE html>This is in progress.")); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
