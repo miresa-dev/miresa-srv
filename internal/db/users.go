14a20c10fd5cda2c4eb450b1454a548bda152134
Kaamkiya <ziam3.1415@gmail.com>
2024-12-26 09:11:21 -0500

    refactor: Move user operations to a separate file

---
+++ internal/db/users.go
@@ -0,0 +1,67 @@
+package db
+
+func GetUser(id string) (user User, err error) {
+	var temp any
+	err = db.QueryRow(`SELECT * FROM users WHERE id=$1`, id).Scan(&user.ID, &user.SID, &user.Name, &user.PasswordHash, &user.Score, &user.Bio, &user.Joined, &user.IsAdmin, &user.IsBanned, &temp)
+	return user, err
+}
+
+func GetUsers(limit int, offset int) (users []User, err error) {
+	rows, err := db.Query(`SELECT * FROM users LIMIT $1 OFFSET $2`, limit, offset)
+	if err != nil {
+		return users, err
+	}
+	for rows.Next() {
+		var user User
+		var temp any
+		if err := rows.Scan(&user.ID, &user.SID, &user.Name, &user.PasswordHash, &user.Score, &user.Bio, &user.Joined, &user.IsAdmin, &user.IsBanned, &temp); err != nil {
+			return users, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
+
+func AddUser(user User) error {
+	_, err := db.Exec(
+		`INSERT INTO users(id, sid, name, pw_hash, score, bio, joined, is_admin, is_banned, items) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`,
+		user.ID,
+		user.SID,
+		user.Name,
+		user.PasswordHash,
+		0,
+		"",
+		user.Joined,
+		false,
+		false,
+		user.Items,
+	)
+	return err
+}
+
+func SetUserBio(id, bio string) error {
+	_, err := db.Exec(`UPDATE users SET bio = $1 WHERE id = $2;`, bio, id)
+	return err
+}
+
+func SetUserPasswordHash(id, passwordHash string) error {
+	_, err := db.Exec(`UPDATE users SET pw_hash = $1 WHERE id = $2;`, passwordHash, id)
+	return err
+}
+
+func SetUserName(id, name string) error {
+	_, err := db.Exec(`UPDATE users SET name = $1 WHERE id = $2`, name, id)
+	return err
+}
+
+func GetUserBySID(sid string) (user User, err error) {
+	row := db.QueryRow(`SELECT * FROM users WHERE sid = $1`, sid)
+
+	err = row.Scan(&user.ID, &user.SID, &user.Name, &user.PasswordHash, &user.Score, &user.Bio, &user.Joined, &user.IsAdmin, &user.IsBanned, &user.Items)
+	return user, err
+}
+
+func SetUserSID(id, sid string) error {
+	_, err := db.Exec(`UPDATE users SET sid = $1 WHERE id = $2`, sid, id)
+	return err
+}
